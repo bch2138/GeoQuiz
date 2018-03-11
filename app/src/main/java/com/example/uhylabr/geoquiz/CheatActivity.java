8c5f83e9cf22d705511a0a7deaa71268ca992dc7
UHYLABR <bch2138@columbia.edu>
2018-03-11 12:24:55 -0400

    Version2

--- app/src/main/java/com/example/uhylabr/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/uhylabr/geoquiz/CheatActivity.java
@@ -17,10 +17,10 @@ public class CheatActivity extends AppCompatActivity {
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
 
-    public static Intent newIntent(Context packageContext, boolean answerIsTrue)
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue){
         Intent intent = new Intent (packageContext, CheatActivity.class);
         intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
-        return intent;
+        return intent;}
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
